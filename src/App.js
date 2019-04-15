e55c3fd1c1a38e7e91f1a764280d983dc88e0088
Agueda Perez <aguedapm@yahoo.com>
2019-04-15 16:53:33 +0200

    Quest Post solved

--- src/App.js
+++ src/App.js
@@ -1,26 +1,109 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
+
+
 class App extends Component {
+
+  state = {
+    name: '',
+    poster: '',
+    comment: '',
+  }
+
+  onChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    })
+   }
+
+   submitForm = (e) => {
+    e.preventDefault();
+    const config = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(this.state),
+     };
+    
+    const url = "http://campus-bordeaux.ovh:3001/api/quests/movies/";
+
+     fetch(url, config) 
+      .then(res => res.json())
+      .then(res => {
+        if (res.error) {
+          alert(res.error);
+        } else {
+          alert(`Votre film a bien été ajouté avec l'ID ${res}!`);
+        }
+      })
+      
+      .catch(e => {
+        console.error(e);
+        alert('Erreur lors de l\'ajout du film');
+      });
+   }
+
+   
   render() {
+
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Poste ton film
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+
         </header>
-      </div>
+        <div className="FormFilm">
+          <h1>Vote pour ton film préféré!</h1>
+
+          <form onSubmit={this.submitForm}>
+            <fieldset>
+              <legend>Informations sur le film</legend>
+              <div className="form-data">
+                <label htmlFor="name">Nom</label>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  onChange={this.onChange}
+                  value={this.state.name}
+                />
+              </div>
+
+              <div className="form-data">
+                <label htmlFor="poster">Poster</label>
+                <input
+                  type="text"
+                  id="poster"
+                  name="poster"
+                  onChange={this.onChange}
+                  value={this.state.poster}
+                />
+              </div>
+
+              <div className="form-data">
+                <label htmlFor="comment">Comment</label>
+                <input
+                  type="comment"
+                  id="comment"
+                  name="comment"
+                  onChange={this.onChange}
+                  value={this.state.comment}
+                />
+                </div>
+              <hr />
+              <div className="form-data">
+                <input type="submit" value="Envoyer" />
+              </div>
+              </fieldset>
+            </form>
+          </div>
+        </div>
     );
   }
 }
